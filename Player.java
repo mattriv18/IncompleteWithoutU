196e0fed824907a16f577bf92891be3cdfd48809
Matthew Rivera <mriver25@binghamton.edu>
2019-02-09 21:32:48 -0500

    add player

---
+++ Player.java
@@ -0,0 +1,108 @@
+package ctrl;
+
+import java.awt.Image;
+import java.awt.event.KeyEvent;
+import javax.swing.ImageIcon;
+
+public class Player {
+
+    private int dx;
+    private int dy;
+    private int x = 40;
+    private int y = 60;
+    private int w;
+    private int h;
+    private Image image;
+    private boolean black;
+
+    public Player() {
+        loadImage();
+    }
+
+    private void loadImage() {
+        
+    	if(black) {
+    		ImageIcon ii = new ImageIcon("src/resources/player1.png");
+            image = ii.getImage(); 
+    	} else {
+    		ImageIcon ii = new ImageIcon("src/resources/player2.png");
+            image = ii.getImage(); 
+            
+    	}
+        w = image.getWidth(null);
+        h = image.getHeight(null);
+    }
+
+    public void move() {
+        
+        x += dx;
+        y += dy;
+    }
+
+    public int getX() {
+        
+        return x;
+    }
+
+    public int getY() {
+        
+        return y;
+    }
+    
+    public int getWidth() {
+        
+        return w;
+    }
+    
+    public int getHeight() {
+        
+        return h;
+    }    
+
+    public Image getImage() {
+        
+        return image;
+    }
+
+    public void keyPressed(KeyEvent e) {
+
+        int key = e.getKeyCode();
+
+        if (key == KeyEvent.VK_LEFT) {
+            dx = -2;
+        }
+
+        if (key == KeyEvent.VK_RIGHT) {
+            dx = 2;
+        }
+
+        if (key == KeyEvent.VK_UP) {
+            dy = -2;
+        }
+
+        if (key == KeyEvent.VK_DOWN) {
+            dy = 2;
+        }
+    }
+
+    public void keyReleased(KeyEvent e) {
+        
+        int key = e.getKeyCode();
+
+        if (key == KeyEvent.VK_LEFT) {
+            dx = 0;
+        }
+
+        if (key == KeyEvent.VK_RIGHT) {
+            dx = 0;
+        }
+
+        if (key == KeyEvent.VK_UP) {
+            dy = 0;
+        }
+
+        if (key == KeyEvent.VK_DOWN) {
+            dy = 0;
+        }
+    }
+}
